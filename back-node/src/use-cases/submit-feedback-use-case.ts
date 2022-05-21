255946fffe94c6ed669c6b2926c79056f056d32c
Gustavo Alexandre da Silva Carvalho <gustavoalexandrescarvalho@gmail.com>
2022-05-20 21:23:07 -0300

    Adicionando jest

--- back-node/src/use-cases/submit-feedback-use-case.ts
+++ back-node/src/use-cases/submit-feedback-use-case.ts
@@ -15,14 +15,28 @@ export class SubmitFeedbackUseCase {
   async execute(request: SubmitFeedbackUseCaseRequest) {
     const { type, comment, screenshot } = request;
 
+    if (!type) {
+      throw new Error("Type is required");
+    }
+
+    if (!comment) {
+      throw new Error("Comment is required");
+    }
+
+    if (screenshot && !screenshot.startsWith("data:image/png;base64,")) {
+      throw new Error("Screenshot must be a base64 encoded png");
+    }
+
     await this.feedbacksRepository.create({ type, comment, screenshot });
     await this.mailAdapter.sendMail({
       subject: "Feedback submitted",
       body: [
+        `<html>`,
         `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
         `<p>Tipo do feedback: ${type}</p>`,
         `<p>Comentario: ${comment}</p>`,
         `</div>`,
+        `</html>`,
       ].join("\n"),
     });
   }
